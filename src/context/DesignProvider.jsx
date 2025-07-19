18e0c4c29bea0af9f41a4c853d792b453d91137b
soposilverhawk <109133065+soposilverhawk@users.noreply.github.com>
2025-07-19 15:07:45 +0400

    add DesignContext

---
+++ src/context/DesignProvider.jsx
@@ -0,0 +1,33 @@
+// DesignProvider.jsx
+import React, { createContext, useContext } from "react";
+
+const DesignContext = createContext();
+
+function DesignProvider({ children }) {
+    const designStyles = {
+        colors: {
+            primaryPink: "#FD007D",
+            primaryBlue: "#070B80",
+            secondaryPink: "#FD007D80",
+            secondaryGray: "#474747",
+            tertiaryPink: "#FD007D70",
+        },
+        fontSizes: {
+            heading1: "36px",
+            heading2: "24px",
+            text: "16px",
+        }
+    };
+    
+    return (
+        <DesignContext.Provider value={designStyles}>
+            {children}
+        </DesignContext.Provider>
+    );
+}
+
+export { DesignProvider };
+
+
+
+export const useDesign = () => useContext(DesignContext);
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import { DesignProvider } from "./context/DesignProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter basename="">
-      <App />
+      <DesignProvider>
+        <App />
+      </DesignProvider>
     </BrowserRouter>
   </StrictMode>
 );
