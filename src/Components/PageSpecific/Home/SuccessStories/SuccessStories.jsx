fe9f8cadf29983339f8ee5732e7f6912b0d9eac5
soposilverhawk <109133065+soposilverhawk@users.noreply.github.com>
2025-07-20 19:07:52 +0400

    add successStories section

---
+++ src/Components/PageSpecific/Home/SuccessStories/SuccessStories.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import SectionHeading from "../shared/SectionHeading";
+import InfoCard from "../../../Shared/InfoCard/InfoCard";
+import { successStoriesCards } from "../../../../data/home/homeData";
+
+console.log(successStoriesCards)
+
+
+function SuccessStories() {
+  return (
+    <section style={{display: "flex", flexDirection: "column", gap: "8rem"}}>
+      <SectionHeading title="წარმატების ისტორიები" />
+      <div style={{display: "flex", justifyContent: "space-between"}}>
+        {successStoriesCards.map(({id, title, titleBg, cardImg}) => (
+          <InfoCard key={id} variant="success" cardTxt={title} bgColor={titleBg} cardImg={cardImg}/>
+        ))}
+      </div>
+    </section>
+  );
+}
+
+export default SuccessStories;
