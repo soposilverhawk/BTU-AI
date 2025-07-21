ad4393f33845ae030e3965c68ec45a85737134f6
soposilverhawk <109133065+soposilverhawk@users.noreply.github.com>
2025-07-21 22:24:37 +0400

    move timelineItems and principle content into single component

--- src/Components/PageSpecific/About/Principles.jsx
+++ src/Components/PageSpecific/About/Principles.jsx
@@ -1,18 +1,90 @@
 import React from "react";
 import { btuPrinciples } from "../../../data/about/about";
-import ItemDescription from "./ItemDescription";
-import TimelineItem from "./TimelineItem";
+import { useDesign } from "../../../context/DesignProvider";
+import SectionHeading from "../../PageSpecific/Home/shared/SectionHeading";
+import { Typography } from "@mui/material";
 
 function Principles() {
+  const { colors, borderRadius, fontSizes } = useDesign();
+
   return (
     <section
       style={{
         display: "flex",
-        justifyContent: "space-between",
+        flexDirection: "column",
+        gap: "40px",
+        position: "relative",
+        maxWidth: "1245px",
+        width: "100%",
+        marginTop: "9rem"
       }}
     >
-      <TimelineItem principlesData={btuPrinciples} />
-      <ItemDescription principlesData={btuPrinciples} />
+      {btuPrinciples.map(
+        ({ id, titleHighlight, titleCore, principleDesc }, idx) => {
+          const isLast = idx === btuPrinciples.length - 1;
+
+          return (
+            <div
+              key={id}
+              style={{
+                display: "flex",
+                alignItems: "flex-start",
+                gap: "24px",
+                position: "relative",
+              }}
+            >
+              {/* Numbered Circle + Line */}
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                  marginTop: "4px", // aligns vertically with heading
+                }}
+              >
+                <div
+                  style={{
+                    width: "50px",
+                    height: "50px",
+                    border: `3px solid ${colors.primaryBlue}`,
+                    borderRadius: borderRadius.btnCircle,
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    fontSize: fontSizes.heading3,
+                    backgroundColor: "#fff",
+                    color: colors.primaryBlue,
+                  }}
+                >
+                  {idx + 1}
+                </div>
+
+                {!isLast && (
+                  <div
+                    style={{
+                      width: "3px",
+                      height: "200px",
+                      backgroundColor: colors.primaryBlue,
+                      marginTop: "31px",
+                    }}
+                  />
+                )}
+              </div>
+
+              {/* Textual Content */}
+              <div style={{display: "flex", flexDirection: "column", gap: "25px"}}>
+                <SectionHeading
+                  title={titleCore}
+                  isHighlited={true}
+                  highLightTxt={titleHighlight}
+                  borderActive={false}
+                />
+                <Typography variant="body1">{principleDesc}</Typography>
+              </div>
+            </div>
+          );
+        }
+      )}
     </section>
   );
 }
--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import HeroStatic from "../../Components/Shared/Hero/HeroStatic/HeroStatic";
 import Principles from "../../Components/PageSpecific/About/Principles";
+import LayoutWrapper from "../../Components/Global/LayoutWrapper/LayoutWrapper";
 
 function About() {
   return (
     <>
-    <HeroStatic heroTitle="ჩვენს შესახებ" />;
-    <Principles />
+      <HeroStatic heroTitle="ჩვენს შესახებ" />;
+      <LayoutWrapper>
+        <Principles />
+      </LayoutWrapper>
     </>
-  )
+  );
 }
 
 export default About;
