bc5c6d25befe2e7e2ec9772d46d3f343b577661e
soposilverhawk <109133065+soposilverhawk@users.noreply.github.com>
2025-07-22 19:07:35 +0400

    add more styling to StudentsList and heading

--- src/Components/PageSpecific/SuccessStories/StudentsList.jsx
+++ src/Components/PageSpecific/SuccessStories/StudentsList.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 import { studentAchievements } from "../../../data/successStories/success";
+import SectionHeading from "../Home/shared/SectionHeading";
 
 function StudentsList() {
   return (
-    <ul style={{listStyleType: "disc"}}>
+    <section style={{marginTop: "2rem"}}>
+      <SectionHeading
+        title=""
+        isHighlited={true}
+        highLightTxt="კომპიუტერული მეცნიერებისა და ხელოვნური ინტელექტის ფაკულტეტის კურსდამთავრებულთა წარმატება"
+        borderActive={false}
+        width="100%"
+      />
+      <ul style={{listStyleType: "disc", marginTop: "5rem"}}>
       {studentAchievements.map(({ id, student, position, company, linkedIn }) => (
         <li key={id}>
           <a href={linkedIn} target="_blank">
@@ -15,6 +24,7 @@ function StudentsList() {
         </li>
       ))}
     </ul>
+    </section>
   );
 }
 
