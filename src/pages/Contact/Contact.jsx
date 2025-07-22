ef1bd63138e6d78525bb3d8248b427e38f33a258
soposilverhawk <109133065+soposilverhawk@users.noreply.github.com>
2025-07-22 23:26:25 +0400

    add map iframe

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -57,6 +57,15 @@ function Contact() {
               <ContactEntity variant="email" />
             </div>
           </div>
+          {/* map container */}
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1965.178740010314!2d44.73783800485907!3d41.70640403518284!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40447344e8047ced%3A0xb53d8ecd4495903f!2sBusiness%20and%20Technology%20University!5e0!3m2!1sen!2sge!4v1753212189701!5m2!1sen!2sge"
+            width="100%"
+            height="800"
+            allowFullScreen=""
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          ></iframe>
         </section>
       </LayoutWrapper>
     </>
