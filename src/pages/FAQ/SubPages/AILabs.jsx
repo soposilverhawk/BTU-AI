c1f7d2e049dd180c062f9213948c22f622f2aa90
soposilverhawk <109133065+soposilverhawk@users.noreply.github.com>
2025-07-23 16:06:42 +0400

    prepare AILabs page html structure

--- src/pages/FAQ/SubPages/AILabs.jsx
+++ src/pages/FAQ/SubPages/AILabs.jsx
@@ -1,8 +1,25 @@
 import React from "react";
 import HeroStatic from "../../../Components/Shared/Hero/HeroStatic/HeroStatic";
+import HeadingWithSocials from "../../../Components/Shared/HeadingWithSocials/HeadingWithSocials";
+import LayoutWrapper from "../../../Components/Global/LayoutWrapper/LayoutWrapper";
 
 function AILabs() {
-  return <HeroStatic heroTitle="AI ლაბორატორიები" />;
+  return (
+    <>
+      <HeroStatic heroTitle="AI ლაბორატორიები" />;
+      <LayoutWrapper>
+        <div style={{ width: "100%", marginTop: "8rem" }}>
+          <HeadingWithSocials highLightTxt="FAQ / AI ლაბორატორიები" />
+          <section>
+            {/* article container */}
+            <article></article>
+            {/* cards container */}
+            <div></div>
+          </section>
+        </div>
+      </LayoutWrapper>
+    </>
+  );
 }
 
 export default AILabs;
