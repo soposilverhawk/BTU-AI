d1b8ccca8653d011386b411dc2fc06a590b04d05
soposilverhawk <109133065+soposilverhawk@users.noreply.github.com>
2025-07-24 13:45:48 +0400

    add gh-pages dependencies for deployment

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { DesignProvider } from "./context/DesignProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="">
+    <BrowserRouter basename="/BTU-AI/">
       <DesignProvider>
         <App />
       </DesignProvider>
