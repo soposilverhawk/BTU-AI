8f8ae42a53be3d7f50dcdf457e9a791b179b4fb3
soposilverhawk <109133065+soposilverhawk@users.noreply.github.com>
2025-08-08 15:19:59 +0400

    add blog posts pagination

--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -2,16 +2,23 @@ import React, { useState } from "react";
 import FAQSubPagesLayout from "../../Components/PageSpecific/FAQ/FAQSubPagesLayout";
 import BlogList from "../../Components/PageSpecific/Blog/BlogList";
 import blogList from "../../data/blog/blogList.json";
+import Controls from "../../Components/PageSpecific/Blog/Controls/Controls";
+import LayoutWrapper from "../../Components/Global/LayoutWrapper/LayoutWrapper";
 
 function Blog() {
   const [endIdx, setEndIdx] = useState(5);
   let step = 5;
   return (
-    <FAQSubPagesLayout
-      heroTitle="ბლოგი"
-      headingText={false}
-      leftContent={<BlogList blogData={blogList} rangeEndidx={endIdx} />}
-    />
+    <>
+      <FAQSubPagesLayout
+        heroTitle="ბლოგი"
+        headingText={false}
+        leftContent={<BlogList blogData={blogList} rangeEndidx={endIdx} />}
+      />
+      <LayoutWrapper>
+        <Controls dataLength={blogList.length} />
+      </LayoutWrapper>
+    </>
   );
 }
 
